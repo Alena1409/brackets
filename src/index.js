88f7da8bc224d866ec2dfb14811ec2b79fe4abc4
Alena1409 <a77993519@gmail.com>
2024-10-15 15:14:12 +0400

    init: start tast-brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let val of str) {
+    if (bracketsConfig[val]) stack.push(val);
+    if (Object.values(bracketsConfig).includes(val)) {
+      if (stack.length === 0 || bracketsConfig[stack.pop()] !== val) return false
+    }
+  }
+  return stack.length === 0;
 }
